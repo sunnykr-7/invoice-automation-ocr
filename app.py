211ad54f94585ccc05fe37504fc1f287bad25100
Sunny Kumar <129001632+sunnykr-7@users.noreply.github.com>
2026-05-11 20:56:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ st.title("📄 Invoice Automation Tool")
 uploaded_file = st.file_uploader("Upload Invoice (PDF/JPG/PNG)")
 
 if uploaded_file:
-    # Save file with correct extensionssssssssssssss
+    # Save file with correct extensions
+    # save
     ext = uploaded_file.name.split(".")[-1]
     file_path = f"temp_file.{ext}"
 
